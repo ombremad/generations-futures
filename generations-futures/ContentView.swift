cfd3fa2b5a0970541d80039e092c681253b38e91
Anne Ferret <anne@anneferret.eu>
2025-06-02 13:49:44 +0200

    empty xcode project

---
+++ generations-futures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  generations-futures
+//
+//  Created by Anne Ferret on 02/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ generations-futures/generations_futuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  generations_futuresApp.swift
+//  generations-futures
+//
+//  Created by Anne Ferret on 02/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct generations_futuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
