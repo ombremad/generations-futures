1cd2c882889a0cf52283bb22543a2cb5f26bcf37
Anne Ferret <anne@anneferret.eu>
2025-06-04 17:03:42 +0200

    first version of ListingAnnoncesView

---
+++ generations-futures/Views/ListingAnnoncesView.swift
@@ -0,0 +1,97 @@
+//
+//  ListingAnnoncesView.swift
+//  generations-futures
+//
+//  Created by Anne Ferret on 04/06/2025.
+//
+
+import SwiftUI
+
+struct ListingAnnoncesView: View {
+    var body: some View {
+        ScrollView {
+                        
+            VStack {
+                
+                SearchBar()
+                    .padding()
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 0) {
+                        SimpleBadge(
+                            thematique: "Filtres",
+                            badge: "rectangle.stack",
+                            highlighted: true
+                        )
+                        SimpleBadge(
+                            thematique: "Un thème avec un nom plus long",
+                            badge: "popcorn.fill"
+                        )
+                        SimpleBadge()
+                        SimpleBadge()
+                        SimpleBadge()
+                        SimpleBadge()
+                        SimpleBadge()
+                        SimpleBadge()
+                    }
+                }
+                .frame(height: 90)
+                
+                SimpleSectionTitle(title: "Annonces près de vous")
+                .padding(.horizontal)
+                                                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack(spacing: 24) {
+                        AnnonceCardBig()
+                        AnnonceCardBig(
+                            titre: "Un café et parler",
+                            thematique: "Discussion",
+                            auteur: "Jean-Paul",
+                            lieu: "Roubaix",
+                            illustration: "samples/coffee"
+                        )
+                        AnnonceCardBig(
+                            titre: "Le nouveau Avatar !",
+                            thematique: "Sorties",
+                            auteur: "Béatrice",
+                            lieu: "Marseille",
+                            illustration: "samples/cinema"
+                        )
+                        AnnonceCardBig()
+                        AnnonceCardBig()
+                        AnnonceCardBig()
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom, 30)
+                }
+                
+                SimpleSectionTitle(title: "Posté récemment")
+                .padding(.horizontal)
+                
+                VStack(spacing:18) {
+                    AnnonceCardSmall()
+                    AnnonceCardSmall(
+                        titre: "Un café et parler",
+                        thematique: "Discussion",
+                        auteur: "Jean-Paul",
+                        illustration: "samples/coffee"
+                    )
+                    AnnonceCardSmall(
+                        titre: "Le nouveau Avatar !",
+                        thematique: "Sorties",
+                        auteur: "Béatrice",
+                        illustration: "samples/cinema"
+                    )
+                }
+                .padding(.horizontal)
+                                
+            }
+            .font(Font.custom("Poppins-Regular", size: 12))
+            .foregroundStyle(Color("Grey-900"))
+        }
+    }
+}
+
+#Preview {
+    ListingAnnoncesView()
+}
