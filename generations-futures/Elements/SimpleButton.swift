54d977cd41313ecc3143195ee77a6458a14cb9c6
Anne Ferret <anne@anneferret.eu>
2025-06-06 09:12:05 +0200

    improved SimpleButton

--- generations-futures/Elements/SimpleButton.swift
+++ generations-futures/Elements/SimpleButton.swift
@@ -16,13 +16,16 @@ struct SimpleButton: View {
 
         Button(action: {}, label: {
             Text(content)
+                .frame(maxWidth: 256)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: cornerRadiusBig)
+                        .fill(highlighted ? Color("Red-500") : Color("Grey-500"))
+                }
                 .font(Font.custom("Poppins-Regular", size: 12))
                 .foregroundStyle(Color("Grey-50"))
-                .frame(maxWidth: 256)
+                .contentShape(Rectangle())
         })
-        .padding()
-        .background(highlighted ? Color("Red-500") : Color("Grey-500"))
-        .cornerRadius(cornerRadiusBig)
         .shadow(color: Color("Grey-300"), radius: shadowAmount, x: 0, y: 4)
         
     }
