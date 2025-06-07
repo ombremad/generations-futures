68ad9dc04d697fbf018d1e898abcbc72f32748b0
Carla <alicyarose@gmail.com>
2025-06-07 16:14:22 +0200

    poursuite du questionnaire

---
+++ generations-futures/Fonts/Questionnaire_View_4.swift
@@ -0,0 +1,18 @@
+//
+//  Questionnaire_4.swift
+//  generations-futures
+//
+//  Created by Apprenant125 on 07/06/2025.
+//
+
+import SwiftUI
+
+struct Questionnaire_View_4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Questionnaire_View_4()
+}
---
+++ generations-futures/Views/Questionnaire_Model.swift
@@ -0,0 +1,8 @@
+//
+//  Questionnaire_Model.swift
+//  generations-futures
+//
+//  Created by Apprenant125 on 07/06/2025.
+//
+
+import Foundation
