14907613ea9d133283f6b54df7849a0d135ef0ad
Carla <alicyarose@gmail.com>
2025-06-07 18:48:25 +0200

    Fin questionnaire_6 + mise en place questionnaire_6_Date précise

---
+++ generations-futures/Views/Questionnaire_View_6.swift
@@ -0,0 +1,20 @@
+//
+//  Questionnaire_View_6.swift
+//  generations-futures
+//
+//  Created by Apprenant125 on 07/06/2025.
+//
+
+import SwiftUI
+
+struct Questionnaire_View_6: View {
+    var body: some View {
+        VStack {
+            HeaderQuestionnaire(num: 6, titre: "Quand êtes vous disponible pour cette expérience?")
+        }
+    }
+}
+
+#Preview {
+    Questionnaire_View_6()
+}
