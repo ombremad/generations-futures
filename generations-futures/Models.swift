a51503df6c993caea8d96bf28bdd3cc3bbe17650
Nicodeur <codewithnico@icloud.com>
2025-06-10 16:48:41 +0200

    AnnoncedetailView Map (no finish)

---
+++ generations-futures/Models.swift
@@ -0,0 +1,18 @@
+//
+//  Models.swift
+//  generations-futures
+//
+//  Created by Apprenant156 on 10/06/2025.
+//
+
+import SwiftUI
+
+struct Models: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Models()
+}
