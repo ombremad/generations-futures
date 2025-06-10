be14ff0e2dbda8a344607dc17e41468d0fadb716
Noa Cengarle <noacengarle@gmail.com>
2025-06-10 16:56:26 +0200

    ProfilEditionView loop hobbies

---
+++ generations-futures/Views/HobbyButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  HobbyButtonView.swift
+//  generations-futures
+//
+//  Created by Noa Cengarle on 10/06/2025.
+//
+
+import SwiftUI
+
+struct HobbyButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HobbyButtonView()
+}
