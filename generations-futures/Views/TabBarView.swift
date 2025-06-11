5751106b7dd7c4a64b78ca08e165f8276adefae4
Noa Cengarle <noacengarle@gmail.com>
2025-06-11 15:53:26 +0200

    TabBarView

---
+++ generations-futures/Views/TabBarView.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarView.swift
+//  generations-futures
+//
+//  Created by Noa Cengarle on 11/06/2025.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        TabView {
+            Tab("Annonces", systemImage: "newspaper.fill") {
+                ListingAnnoncesView()
+            }
+            
+            Tab("Messagerie", systemImage: "message.fill") {
+                ListingMessagesView()
+            }
+            
+            Tab("Mon profil", systemImage: "person.fill") {
+                ProfilEditionView()
+            }
+        }
+        .tabViewStyle(.sidebarAdaptable)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
