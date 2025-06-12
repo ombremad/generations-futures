063365ca681b49b7f50cedc01cfaf6852b9833db
Anne Ferret <anne@anneferret.eu>
2025-06-12 10:30:45 +0200

    début de données de démo pour les profils et les annonces

--- generations-futures/DefaultValues.swift
+++ generations-futures/DefaultValues.swift
@@ -27,7 +27,46 @@ let shadowColor: Color = Color("Grey-200")
 let cornerRadiusSmall: CGFloat = 12
 let cornerRadiusBig: CGFloat = 90
 
-// Conversation de démo
+// Données de démo
+// SVP NE CHANGEZ PAS L'ORDRE DES DONNÉES DANS LES TABLEAUX !
+// Des trucs sont codés en dur. Si vous voulez rajouter des données, faites-le à la fin des tableaux.
+
+struct Profile: Hashable {
+    var name: String
+    var surname: String
+    var profilePicture: ImageResource
+    var lieu: String
+    var yearOfBirth: Int
+    var generation: String
+    var description: String
+}
+
+let profiles: [Profile] = [
+    Profile(
+        name: "Robert",
+        surname: "Dupont",
+        profilePicture: .robert,
+        lieu: "Lille",
+        yearOfBirth: 1961,
+        generation: "Baobab",
+        description: "Un roadtrip au Brésil m’a suffi pour tomber amoureux de la samba. \n17 ans d’expériences dans les pattes!")
+]
+
+struct Annonce: Hashable {
+    var titre: String
+    var description: String
+    var illustration: ImageResource
+    var thematique: Hobby
+    var author: Profile
+}
+
+let annonces: [Annonce] = [
+    Annonce(titre: "Un café et parler",
+            description: "Parler de tout et de rien, de mon roadtrip au Brésil, de la samba, des plantes, des hobbies en général...",
+            illustration: .Samples.coffee,
+            thematique: hobbies[12],
+            author: profiles[0])
+    ]
 
 struct Message: Hashable {
     var text: String
@@ -66,7 +105,7 @@ struct Hobby: Hashable {
 
 let hobbies: [Hobby] = [
     Hobby(icon: "figure.run", label: "Sport"),
-    Hobby(icon: "theatermasks.fill", label: "théâtre"),
+    Hobby(icon: "theatermasks.fill", label: "Théâtre"),
     Hobby(icon: "music.note", label: "Musique"),
     Hobby(icon: "book.fill", label: "Lecture"),
     Hobby(icon: "pencil", label: "Écriture"),
@@ -76,5 +115,7 @@ let hobbies: [Hobby] = [
     Hobby(icon: "popcorn.fill", label: "Cinéma"),
     Hobby(icon: "airplane", label: "Voyages"),
     Hobby(icon: "paintbrush.pointed.fill", label: "Peinture"),
-    Hobby(icon: "tortoise.fill", label: "Animaux")
+    Hobby(icon: "tortoise.fill", label: "Animaux"),
+    Hobby(icon: "bubble.left.and.text.bubble.right.fill", label: "Discussion"),
+    Hobby(icon: "questionmark.circle.dashed", label: "Autres")
    ]
