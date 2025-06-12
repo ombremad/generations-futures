0fa447384c51a130a6ca975d87b8225a785187bb
Nicodeur <codewithnico@icloud.com>
2025-06-12 13:46:22 +0200

    QuestionnaireView3 & QuestionnaireView4 (ajout d'une image profil, et ajout du text editor)

--- generations-futures/Views/Questionnaire_View_3.swift
+++ generations-futures/Views/Questionnaire_View_3.swift
@@ -6,45 +6,57 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct Questionnaire_View_3: View {
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectImage: Image?
+
     var body: some View {
         ZStack {
             Color.almostWhite
                 .ignoresSafeArea()
-            
-            VStack{
+
+            VStack {
                 HeaderQuestionnaire(num: 3, titre: "Une image pour votre thème?")
-                
-                Button {
-                    // pouvoir choisir une photo
-                } label: {
-                    VStack{
+
+                PhotosPicker(
+                    selection: $pickerItem,
+                    matching: .images,
+                    photoLibrary: .shared()
+                ) {
+                    VStack {
                         Image(systemName: "plus")
-                               .font(.system(size: 33))
-                               .foregroundStyle(.almostWhite)
-                               .padding(20)
-                               .background{
-                                   Circle()
-                                       .fill(.grey500)
-                                       
-                               }
+                            .font(.system(size: 33))
+                            .foregroundStyle(.almostWhite)
+                            .padding(20)
+                            .background {
+                                Circle()
+                                    .fill(.grey500)
+                            }
                         Text("Ajouter une image")
                             .font(Font.custom("Poppins-Regular", size: 14))
                             .foregroundStyle(.grey500.opacity(0.5))
                             .padding(.top, 2)
-
                     }
-                }.padding(.top, 100)
+                }
+                .padding(.top, 100)
 
                 Spacer()
-                
-                SuivantButton()
-                
 
+                SuivantButton()
             }
         }
-       
+        .onChange(of: pickerItem) {
+            Task {
+                if let data = try? await pickerItem?.loadTransferable(type: Data.self),
+                   let uiImage = UIImage(data: data) {
+                    selectImage = Image(uiImage: uiImage)
+                }
+            }
+        }
+
+
     }
 }
 
