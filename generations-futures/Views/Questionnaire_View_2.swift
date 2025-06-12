b9205670feb00a8bc2a7b80709a030e5d2da53d7
alicyarose@gmail.com <alicyarose@gmail.com>
2025-06-12 07:54:24 +0200

    confection du ForEach liste questionaire 2

--- generations-futures/Views/Questionnaire_View_2.swift
+++ generations-futures/Views/Questionnaire_View_2.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct Questionnaire_View_2: View {
+    
+    var themes : [String] = ["Artistique","Café","Atelier","Visites","Sport","Discussion"]
+    
     var body: some View {
         ZStack{
             Color.almostWhite
@@ -15,12 +18,38 @@ struct Questionnaire_View_2: View {
             VStack{
                 HeaderQuestionnaire(num: 2, titre: "Quel est le thème?")
                 
+                ForEach(themes, id: \.self) { theme in
+                    ZStack {
+                        
+                        Text(theme)
+                            .font(Font.custom("Poppins-Regular", size: 12))
+                            .foregroundStyle(.grey900)
+                            .frame(width: 240, height: 50)
+                            .background{
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(.grey50)
+                            }
+                        Circle()
+                            .frame(width: 40)
+                            .foregroundStyle(.white)
+                            .padding(.trailing, 150)
+                            .padding(.bottom,25)
+                        
+                        Image(systemName: "building.columns")
+                            .foregroundStyle(.red500)
+                            .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                    }
+                }
                 
+                Spacer()
                 SuivantButton()
             }
             
         }
-       
+        
     }
 }
 
