61e1b272209eae211559e09c95f02931d74d30e2
Anne Ferret <anne@anneferret.eu>
2025-06-12 09:34:24 +0200

    updated ContentView + info.plist

--- generations-futures/ContentView.swift
+++ generations-futures/ContentView.swift
@@ -10,31 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        NavigationStack {
-            List {
-                NavigationLink(destination: AnnonceDetailView()) {
-                    Text("AnnonceDetailView")
-                }
-                NavigationLink(destination: ListingAnnoncesView()) {
-                    Text("ListingAnnoncesView")
-                }
-                NavigationLink(destination: ListingMessagesView()) {
-                    Text("ListingMessagesView")
-                }
-                NavigationLink(destination: MessagingDetailView()) {
-                    Text("MessagingDetailView")
-                }
-                NavigationLink(destination: ProfilEditionView()) {
-                    Text("ProfilEditionView")
-                }
-                NavigationLink(destination: ProfilView()) {
-                    Text("ProfilView")
-                }
-                NavigationLink(destination: Questionaire_View_1()) {
-                    Text("Questionaire_View_1")
-                }
-            }
-        }
+        TabBarView()
     }
 }
 
